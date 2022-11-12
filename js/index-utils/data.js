33e162523551a613d11d082b3169063a3217ce0a
Ruxandra Boghean <ruxandraboghean@gmail.com>
2022-11-12 16:29:49 +0200

    small screen

--- js/index-utils/data.js
+++ js/index-utils/data.js
@@ -20,4 +20,25 @@ export const products = [
     img: "images/painting3.jpg",
     inCart: 0,
   },
+  {
+    id: 4,
+    name: "Watercolor Girl",
+    price: 50,
+    img: "images/painting4.jpg",
+    inCart: 0,
+  },
+  {
+    id: 5,
+    name: "Watercolor forest",
+    price: 90,
+    img: "images/painting5.jpg",
+    inCart: 0,
+  },
+  {
+    id: 6,
+    name: "Watercolor mist",
+    price: 100,
+    img: "images/painting6.jpg",
+    inCart: 0,
+  },
 ];
